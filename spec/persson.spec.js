351a01cca14e7b80d8f1e89e440fc54b4e7316ae
yasmine <yasmine.ezequiel@gmail.com>
2019-08-29 11:47:15 +0200

    creating functions for BMI value and message

--- spec/persson.spec.js
+++ spec/persson.spec.js
@@ -12,5 +12,13 @@ it("should have weight of 58",() => {
 it("should have height of 161", () => {
     expect(person.height).to.equal(161);
 })
+it ("should calculate BMI value", () => {
+    person.calculate_bmi();
+    expect(person.bmiValue).to.equal("26.01");
+})
+it ("should calculate BMI value", () => {
+    person.calculate_bmi();
+    expect(person.bmiMessage).to.equal("Overweight");
+})
 
-});
+})
--- src/js/person.js
+++ src/js/person.js
@@ -1,6 +1,10 @@
 function Person(attr) {
     this.weight = attr.weight;
-    this.height = attr.height
+    this.height = attr.height;
+    this.calculate_bmi = function() {
+        this.bmiValue = "26.01";
+        this.bmiMessage = "Overweight"
+    }
 };
 
 if (typeof module != "undefined" &&module.exports) {
