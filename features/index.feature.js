ed5037b8e6ecb5d744ca6a93b24631c602a4f22d
yasmine <yasmine.ezequiel@gmail.com>
2019-09-19 14:29:31 +0200

    Started to add imperial method for the calculator

--- features/index.feature.js
+++ features/index.feature.js
@@ -3,7 +3,7 @@ const { expect } = require('chai');
 const BrowserHelpers = require('e2e_training_wheels')
 const browser = new BrowserHelpers()
 
-describe('User can calculate BMI', () => {
+describe('User can calculate BMI with the metric units', () => {
     before(async () => {
         await browser.init()
         await browser.visitPage('http://localhost:8080/')
@@ -25,5 +25,29 @@ describe('User can calculate BMI', () => {
         await browser.clickOnButton("button")
         let content = await browser.getContent("span[id='display_value']")
         expect(content).to.eql('Your BMI is 27.76');
+    });
+});
+describe('User can calculate BMI using imperial system', () => {
+    before(async () => {
+        await browser.init()
+        await browser.visitPage('http://localhost:8080/')
+    });
+
+    beforeEach(async () => {
+        await browser.page.reload();
+    });
+
+    after(async () => {
+        await browser.close();
+    });
+
+    it('by inputing his weight and height', async () => {
+        await browser.selectOption("select[id='my_select']", {option: 'imperial' })
+        await browser.fillIn("input[id='weight-in-lbs']", { with: "170" })
+        await browser.fillIn("input[id='height-in-in']", { with: "70" })
+        await browser.clickOnButton("button")
+        let content = await browser.getContent("span[id='display_value']")
+
+        expect(content).to.eql('Your BMI is 24.39');
     });
 });
